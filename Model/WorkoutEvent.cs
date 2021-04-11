4623c77dc19457c7c338222ebfe806835e8a006f
NemesLaszlo <wow.laszlo@gmail.com>
2021-04-11 16:12:54 +0200

    WorkoutEvent model init

---
+++ Model/WorkoutEvent.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    public class WorkoutEvent
+    {
+        [Key]
+        public Guid WorkoutEvent_Id { get; set; }
+        [Required]
+        public string Title { get; set; }
+        [Required]
+        public DateTime Date { get; set; }
+        public string Description { get; set; }
+        [Required]
+        public string Category { get; set; }
+        public string City { get; set; }
+        public string Place { get; set; }
+        public bool IsCancelled { get; set; }
+    }
+}
