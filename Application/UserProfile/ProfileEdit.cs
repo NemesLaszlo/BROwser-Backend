3e417a74b0ee97f25a1c0d0395fac2b9482f665d
NemesLaszlo <wow.laszlo@gmail.com>
2021-06-08 16:45:52 +0200

    Profile Update / Modify

---
+++ Application/UserProfile/ProfileEdit.cs
@@ -0,0 +1,72 @@
+﻿using Application.Core;
+using Application.Interfaces;
+using Database;
+using FluentValidation;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Application.UserProfile
+{
+    /// <summary>
+    /// Modify / Update a User's profile
+    /// </summary>
+    public class ProfileEdit
+    {
+        /// <summary>
+        /// Properties to modify
+        /// </summary>
+        public class Command : IRequest<Result<Unit>>
+        {
+            public string DisplayName { get; set; }
+            public string Bio { get; set; }
+        }
+
+        /// <summary>
+        /// Validate the value for the modification
+        /// </summary>
+        public class CommandValidator : AbstractValidator<Command>
+        {
+            public CommandValidator()
+            {
+                RuleFor(x => x.DisplayName).NotEmpty();
+            }
+        }
+
+        /// <summary>
+        /// Profile modification - business operation handler
+        /// </summary>
+        public class Handler : IRequestHandler<Command, Result<Unit>>
+        {
+            private readonly DataContext _context;
+            private readonly IUserAccessor _userAccessor;
+
+            public Handler(DataContext context, IUserAccessor userAccessor)
+            {
+                _userAccessor = userAccessor;
+                _context = context;
+            }
+
+            // Modify a Profile
+            public async Task<Result<Unit>> Handle(Command request, CancellationToken cancellationToken)
+            {
+                var user = await _context.Users.FirstOrDefaultAsync(x => x.UserName == _userAccessor.GetUsername());
+
+                user.Bio = request.Bio ?? user.Bio;
+                user.DisplayName = request.DisplayName ?? user.DisplayName;
+
+                _context.Entry(user).State = EntityState.Modified;
+
+                var success = await _context.SaveChangesAsync() > 0;
+
+                if (success) return Result<Unit>.Success(Unit.Value);
+                return Result<Unit>.Failure("Problem updating profile");
+            }
+        }
+    }
+}
--- BROwser-API/Controllers/ProfilesController.cs
+++ BROwser-API/Controllers/ProfilesController.cs
@@ -9,10 +9,26 @@ namespace BROwser_API.Controllers
 {
     public class ProfilesController : BaseApiController
     {
+        /// <summary>
+        /// Get the user's profile to get more information about him/her.
+        /// </summary>
+        /// <param name="username">User's username to get the details</param>
+        /// <returns>Response handler result - with the profile informations</returns>
         [HttpGet("{username}")]
         public async Task<IActionResult> GetProfile(string username)
         {
             return HandleResult(await Mediator.Send(new ProfileDetails.Query { Username = username }));
+        }
+
+        /// <summary>
+        /// Modify / Update a User's profile
+        /// </summary>
+        /// <param name="command">Bio and/or DisplayName to modify</param>
+        /// <returns>Response handler result</returns>
+        [HttpPut]
+        public async Task<IActionResult> Edit(ProfileEdit.Command command)
+        {
+            return HandleResult(await Mediator.Send(command));
         }
     }
 }
